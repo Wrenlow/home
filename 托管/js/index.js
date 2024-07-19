538657feea81d9eb9f91e5808ed4d96adf60887e
Wrenlow <153977469+Wrenlow@users.noreply.github.com>
2024-07-19 18:23:33 +0800

    Delete 托管 directory

--- 托管/js/index.js
+++
@@ -1,9 +0,0 @@
-var img = document.getElementById("img");
-var box = document.getElementById("box");
-var box2 = document.getElementById("box2");
-var vid = document.getElementById("vid");
-img.onclick = function(){
-    box.style.display = "none";
-    box2.style.display = "block";
-    vid.play()
-}
